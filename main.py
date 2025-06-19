346dda8dad8ee4f8e342ecdb95f9b1faf536269d
Lotjarn <sebastian.lotjarn@gmail.com>
2025-06-19 12:01:49 +0200

    Added FPS resource management

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+pygame.init()
 
 def main():
 	for event in pygame.event.get():
     		if event.type == pygame.QUIT:
         		return
-	pygame.init
+	Clock = pygame.time.Clock()
+	dt = 0
 	print ("Starting Asteroids!")
 	print (f"Screen width: {SCREEN_WIDTH}")
 	print (f"Screen height: {SCREEN_HEIGHT}")
@@ -13,6 +15,7 @@ def main():
 	while True:
 		screen.fill("black")
 		pygame.display.flip()
+		dt = Clock.tick(60) / 1000
 
 if __name__ == "__main__": #Ensures the main() is only called when this file is run directly and won't run if it's imported
 	main()
