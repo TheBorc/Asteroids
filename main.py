63293b5ef4c3fafe7fd677cd4aac0b05dbf7d0f7
Lotjarn <sebastian.lotjarn@gmail.com>
2025-06-20 12:15:48 +0200

    Created player sprite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import*
+from circleshape import*
 pygame.init()
 
 def main():
@@ -12,8 +14,10 @@ def main():
 	print (f"Screen width: {SCREEN_WIDTH}")
 	print (f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		dt = Clock.tick(60) / 1000
 
