8d16270a2c4422ca87f7af4173cd6bb0e1fbcb4d
Lotjarn <sebastian.lotjarn@gmail.com>
2025-06-21 11:41:15 +0200

    Bullet CD

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.timer = 0
 	
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,8 +27,10 @@ class Player(CircleShape):
 		self.position += forward * PLAYER_SPEED * dt
 
 	def shoot(self):
-		shot = Shot(self.position.x, self.position.y)
-		shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+		if self.timer <= 0:
+			shot = Shot(self.position.x, self.position.y)
+			shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+			self.timer = PLAYER_SHOOT_CD
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
@@ -41,3 +44,4 @@ class Player(CircleShape):
 			self.move(-dt)
 		if keys[pygame.K_SPACE]:
 			self.shoot()
+		self.timer -= dt
